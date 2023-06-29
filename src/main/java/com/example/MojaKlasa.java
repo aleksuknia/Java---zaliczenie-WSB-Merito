ecacd92d2bac20ba62322f450f413a2e5061ec1a
aleksuknia <137430469+aleksuknia@users.noreply.github.com>
2023-06-29 23:49:12 +0200

    Add files via upload

---
+++ src/main/java/com/example/MojaKlasa.java
@@ -0,0 +1,24 @@
+public class MojaKlasa {
+    // Pola (opcjonalnie)
+    private int pole1;
+    private String pole2;
+
+    // Konstruktor (opcjonalnie)
+    public MojaKlasa(int pole1, String pole2) {
+        this.pole1 = pole1;
+        this.pole2 = pole2;
+    }
+
+    // Metody
+    public void metoda1() {
+        // Implementacja metody 1
+    }
+
+    public String metoda2(int liczba) {
+        // Implementacja metody 2
+        return "Wynik: " + (liczba * 2);
+    }
+
+    // Inne elementy klasy
+
+}
